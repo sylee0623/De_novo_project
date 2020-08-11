99950e24aeda296b160d618eeddda31e79144683
sylee <seoyeon0623@hanyang.ac.kr>
2020-08-11 15:31:31 +0900

    서버에 올릴 때 작업이름, 로그 이름 변경

--- gatk/haplotypecaller.sh
+++ gatk/haplotypecaller.sh
@@ -6,7 +6,7 @@ GATK_bundle=/lustre/export/home/sylee/ucsc_hg19
 
 for ((a=1;a<=5;a++))
 do
-qsub -lvnode=biglab1 -j oe -o HC1_5 -- /lustre/export/home/sylee/programfile/gatk-4.1.7.0/gatk --java-options "-Xmx5g -Xms5g" HaplotypeCaller \
+qsub -lvnode=biglab1 -j oe -o HC${i} -N HC${i} -- /lustre/export/home/sylee/programfile/gatk-4.1.7.0/gatk --java-options "-Xmx5g -Xms5g" HaplotypeCaller \
     --input /lustre/export/home/sylee/Hanchinese/try3/result/gatk/daughter.sorted.markdup.BQSR.bam \
     --output /lustre/export/home/sylee/Hanchinese/try3/result/gatk/HaplotypeCaller/chr${a}.hc.g.vcf.gz \
     --reference ${reference} \
@@ -17,7 +17,7 @@ done;
 
 for ((a=6;a<=10;a++))
 do
-qsub -lvnode=biglab2 -j oe -o HC1_5 -- /lustre/export/home/sylee/programfile/gatk-4.1.7.0/gatk --java-options "-Xmx5g -Xms5g" HaplotypeCaller \
+qsub -lvnode=biglab2 -j oe -o HC${i} -N HC${i} -- /lustre/export/home/sylee/programfile/gatk-4.1.7.0/gatk --java-options "-Xmx5g -Xms5g" HaplotypeCaller \
     --input /lustre/export/home/sylee/Hanchinese/try3/result/gatk/daughter.sorted.markdup.BQSR.bam \
     --output /lustre/export/home/sylee/Hanchinese/try3/result/gatk/HaplotypeCaller/chr${a}.hc.g.vcf.gz \
     --reference ${reference} \
@@ -28,7 +28,7 @@ done;
 
 for ((a=11;a<=22;a++))
 do
-qsub -lvnode=biglab3 -j oe -o HC1_5 -- /lustre/export/home/sylee/programfile/gatk-4.1.7.0/gatk --java-options "-Xmx5g -Xms5g" HaplotypeCaller \
+qsub -lvnode=biglab3 -j oe -o HC${i} -N HC${i} -- /lustre/export/home/sylee/programfile/gatk-4.1.7.0/gatk --java-options "-Xmx5g -Xms5g" HaplotypeCaller \
     --input /lustre/export/home/sylee/Hanchinese/try3/result/gatk/daughter.sorted.markdup.BQSR.bam \
     --output /lustre/export/home/sylee/Hanchinese/try3/result/gatk/HaplotypeCaller/chr${a}.hc.g.vcf.gz \
     --reference ${reference} \
@@ -37,7 +37,7 @@ qsub -lvnode=biglab3 -j oe -o HC1_5 -- /lustre/export/home/sylee/programfile/gat
     --L chr$a
 done;
 
-qsub -lvnode=biglab3 -j oe -o HC1_5 -- /lustre/export/home/sylee/programfile/gatk-4.1.7.0/gatk --java-options "-Xmx5g -Xms5g" HaplotypeCaller \
+qsub -lvnode=biglab3 -j oe -o HC${i} -N HC${i} -- /lustre/export/home/sylee/programfile/gatk-4.1.7.0/gatk --java-options "-Xmx5g -Xms5g" HaplotypeCaller \
     --input /lustre/export/home/sylee/Hanchinese/try3/result/gatk/daughter.sorted.markdup.BQSR.bam \
     --output /lustre/export/home/sylee/Hanchinese/try3/result/gatk/HaplotypeCaller/chrX.hc.g.vcf.gz \
     --reference ${reference} \
